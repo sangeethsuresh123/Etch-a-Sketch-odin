36256a8d9581a954e669e5cb2f51be41dd03565e
Sangeeth Suresh <sangeethsuresh321@gmail.com>
2025-09-25 00:08:17 +0000

    Add user input
    Added functionality to allow user input to define
    dimensions of the grid.

--- scripts.js
+++ scripts.js
@@ -1,27 +1,45 @@
-const cont = document.querySelector('.outer')
 let arr = [];
-let arr1 = [];
-for (let i = 0; i < 16; i++) {
-  arr1[i] = document.createElement('div');
-  arr1[i].classList.add('row');
-  cont.appendChild(arr1[i]);
-}
-arr.push(arr1);
-
-for (let i = 0; i < 16; i++) {
-  let arr2 = [];
-  for (let j = 0; j < 16; j++) {
-    arr2[j] = document.createElement('square');
-    arr2[j].classList.add('square');
-    arr[0][i].appendChild(arr2[j]);
+function createGrid() {
+  for (let i = 0; i < arr.length; i++) { // Iterate through rows
+    for (let j = 0; j < arr[i].length; j++) { // Iterate through columns
+      arr[i][j].remove(); // Access each element
+    }
   }
-  arr.push(arr2);
+  arr = [];
+  let size = prompt("Enter number of squares per side: ", 16);
+  if (size > 100) {
+    alert("Size limit reached(max 100)!");
+    return;
+  }
+  const p = document.querySelector('#info');
+  p.textContent = `Dimensions: ${size}x${size}`;
+  const cont = document.querySelector('.outer')
+  let arr1 = [];
+  for (let i = 0; i < size; i++) {
+    arr1[i] = document.createElement('div');
+    arr1[i].classList.add('row');
+    cont.appendChild(arr1[i]);
+  }
+  arr.push(arr1);
+
+  for (let i = 0; i < size; i++) {
+    let arr2 = [];
+    for (let j = 0; j < size; j++) {
+      arr2[j] = document.createElement('square');
+      arr2[j].classList.add('square');
+      arr[0][i].appendChild(arr2[j]);
+    }
+    arr.push(arr2);
+  }
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      console.log('Element hovered!');
+      item.style.backgroundColor = 'white';
+    });
+  });
 }
 
-const squares = document.querySelectorAll('.square');
-squares.forEach(item => {
-  item.addEventListener('mouseover', () => {
-    console.log('Element hovered!');
-    item.style.backgroundColor = 'lightgray';
-  });
-});
+const button = document.querySelector('#select');
+button.addEventListener('click', createGrid);
+
